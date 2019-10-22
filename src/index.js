b95ea956bbd67f0f8bcbd928eaec19ba7fed7bcc
Zubair <m.a.zubair@accenture.com>
2019-10-22 12:17:51 +0530

    code

--- src/index.js
+++ src/index.js
@@ -57,7 +57,5 @@ setInterval( () => {
    }, 1000);
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
