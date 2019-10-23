c9222f2d250fa875706541e6a8f87b0726b618dd
Zubair <m.a.zubair@accenture.com>
2019-10-23 15:25:12 +0530

    comments added for camparinf with MVI architecture

--- mviarch/src/index.js
+++ mviarch/src/index.js
@@ -9,49 +9,54 @@ const createStore = (reducer) => {
     let internalState;
     let handlers = [];
     return {
-      dispatch: (action) => {
-        internalState = reducer(internalState, action);
-        handlers.forEach(h => { h(); });
-      },
-      subscribe: (handler) => { 
-        handlers.push(handler);
-      },
-      getState: () => internalState
+        dispatch: (action) => {
+            internalState = reducer(internalState, action);
+            handlers.forEach(h => { h(); });
+        },
+        subscribe: (handler) => {
+            handlers.push(handler);
+        },
+        getState: () => internalState
     };
-  };
-  
-  let container = createStore((model = { running: false, time: 0 }, intent) => {
+};
+
+// const update = (model ,intent) => {
+let container = createStore((model = { running: false, time: 0 }, intent) => {
     const updates = {
-      'START': (model) => Object.assign(model, {running: true}),
-      'STOP': (model) => Object.assign(model, {running: false}),
-      'TICK': (model) => Object.assign(model, {time: model.time + (model.running ? 1 : 0)})
+        'START': (model) => Object.assign(model, { running: true }),
+        'STOP': (model) => Object.assign(model, { running: false }),
+        'TICK': (model) => Object.assign(model, { time: model.time + (model.running ? 1 : 0) })
     };
     return (updates[intent] || (() => model))(model);
-  });
-  
-  let view = (m) => {
+});
+
+
+
+let view = (m) => {
     let minutes = Math.floor(m.time / 60);
     let seconds = m.time - (minutes * 60);
-    let secondsFormatted =  `${seconds < 10 ? '0' : ''}${seconds}`;
+    let secondsFormatted = `${seconds < 10 ? '0' : ''}${seconds}`;
     let handler = (event) => {
-      container.dispatch(m.running ? 'STOP' : 'START');
+        // model = update(model, model.running ? 'STOP' : 'START'); 
+        container.dispatch(m.running ? 'STOP' : 'START');
     };
-    
+
     return <div>
-      <p>{minutes}:{secondsFormatted}</p>
-      <button onClick={handler}>{m.running ? 'Stop' : 'Start'}</button>
+        <p>{minutes}:{secondsFormatted}</p>
+        <button onClick={handler} > {m.running ? 'Stop' : 'Start'}</button>
     </div>;
-  };
-  
-  const render = () => {
+}
+
+
+const render = () => {
     ReactDOM.render(view(container.getState()),
-      document.getElementById('root')
+        document.getElementById('root')
     );
-  };
-  container.subscribe(render);
-  
-  setInterval(() => {
+};
+container.subscribe(render);
+
+setInterval(() => {
     container.dispatch('TICK');
-  }, 1000);
+}, 1000);
 
 serviceWorker.unregister();
